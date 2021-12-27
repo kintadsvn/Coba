23362e9a418265469cbfc21b9fc30b0182af6393
kintadsvn <kintaaaw@gmail.com>
2021-12-27 11:35:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="style-index.css">
+    </head>
+    <body bgcolor="#355C7D">
+        <table class="badan_tabel">
+            <tr>
+                <td colspan="3" class="header">
+                    <img src="banner-jatimpark.jpg" >
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" class="minibar">
+                    <marquee scrolldelay="5">Selamat Datang di Website Jawa Timur Park Group!</marquee>
+                </td>
+            </tr>
+            <tr>
+                <td class="leftbar">
+                    <table class="box" align="center">
+                        <tr>
+                            <td>
+                                <img src="logo-jatimpark1.png">
+                                <h3>Jawa Timur Park 1</h3>
+                                <p>
+                                    Taman air, wahana seru, pusat wisata edukasi &#8212<br>
+                                    di Jatim Park 1, Anda bisa menikmati semuanya dan<br>
+                                    masih banyak lagi!
+                                </p>
+                                <a href="jp1.php"><button class="button">Baca selengkapnya!</button></a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td class="centerbar">
+                    <table class="box" align="center">
+                        <tr>
+                            <td>
+                                <img src="logo-jatimpark2.png">
+                                <h3>Jawa Timur Park 2</h3>
+                                <p>
+                                    Temui lebih dari 200 spesies hewan lalu jelajahi<br>
+                                    museum margasatwa terbesar di Indonesia, semua di<br>
+                                    hari yang sama. Penasaran bagaimana caranya?
+                                </p>
+                                <a href="jp2.php"><button class="button">Baca selengkapnya!</button></a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td class="rightbar">
+                    <table class="box" align="center">
+                        <tr>
+                            <td>
+                                <img src="logo-jatimpark3.png">
+                                <h3>Jawa Timur Park 3</h3>
+                                <p>
+                                    Di sini, Anda dapat melakukan beragam aktivitas seru,<br>
+                                    seperti kembali ke zaman dinosaurus, berpose dengan<br>
+                                    selebriti, mempelajari sejarah musik dunia, dan masih banyak lagi!
+                                </p>
+                                <a href="jp3.php"><button class="button">Baca selengkapnya!</button></a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" class="footer">
+                    Kinta Desvina XI-RPL-B | 2021
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
